5bcbfdd228790942b0d2f6a46876e4619e6fce89
Matt <mhgarry92@gmail.com>
2023-06-18 15:13:26 -0400

    added Landing page

---
+++ portfolio/src/pages/Landing.jsx
@@ -0,0 +1,107 @@
+import { motion } from "framer-motion";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import ProfilePic from "../assets/images/profilepic/profilepic.png";
+import MediaIcons from "../components/MediaIcons";
+import useMediaQuery from "../hooks/useMediaQuery";
+
+const Landing = ({ setSelectedPage }) => {
+	const LargeScreen = useMediaQuery("(min-width: 1060px)");
+	return (
+		<section
+			id='home'
+			className='md:flex md:justify-between md:items-center gap-16 md:h-full py-10'>
+			<div className='basis-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2'>
+				{LargeScreen ? (
+					<div
+						className='opacity-90 before:rounded-3xl relative z-0 ml-20 before:absolute before:-top-20 before:-left-20
+            before:w-full before:max-w-[600px] md:before:max-w-[600px] before:h-full before:border-2 before:border-pink before:z-[-1]'>
+						<img
+							alt='profile'
+							className='opacity-90 rounded-3xl transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]'
+							src={ProfilePic}
+						/>
+					</div>
+				) : (
+					<img
+						alt='profile'
+						className='z-10 w-full max-w-[400px] md:max-w-[600px]'
+						src={ProfilePic}
+					/>
+				)}
+			</div>
+
+			<div className='z-30 basis-2/5 mt-12 md:mt-32'>
+				<motion.div
+					initial='hidden'
+					whileInView='visible'
+					viewport={{ once: true, amount: 0.5 }}
+					transition={{ duration: 1.5 }}
+					variants={{
+						hidden: {
+							opacity: 0,
+							x: -50,
+						},
+						visible: {
+							opacity: 1,
+							x: 0,
+						},
+					}}>
+					<p className='text-6xl z-10 text-center md:text-start'>
+						Matthew {""}
+						<span
+							className='xs:relative xs:text-pink xs:font-semibold z-20 xs:before:content-brush
+              before:absolute before:-left-[25px] before:-top-[70px] before:z-[-1]'>
+							Garry
+						</span>
+					</p>
+
+					<p className='mt-10 mb-7 text-sm text-center md:text-start'>
+						I'm a full stack web developer. My passion is making beautiful,
+						fast, responsive, and creative web applications. Come visit my
+						virtual playground.
+					</p>
+				</motion.div>
+				<motion.div
+					className='flex mt-5 justify-center md:justify-start'
+					initial='hidden'
+					whileInView='visible'
+					viewport={{ once: true, amount: 0.5 }}
+					transition={{ delay: 0.2, duration: 0.5 }}
+					variants={{
+						hidden: { opacity: 0, x: -50 },
+						visible: { opacity: 1, x: 0 },
+					}}>
+					<AnchorLink
+						className='bg-primary-blue text-pink rounded-sm py-3 px-7 font-semibold hover:bg-deep-blue hover:text-white transition duration-500'
+						onClick={() => setSelectedPage("contact")}
+						href='#contact'>
+						Contact Me
+					</AnchorLink>
+					<AnchorLink
+						className='rounded-r-sm bg-primary-blue
+          py-0.5 px-0.5'
+						onClick={() => setSelectedPage("contact")}
+						href='#contact'>
+						<div className='bg-primary-blue hover:text-white transition duration-500 w-full h-full flex items-center justify-center px-10'>
+							Let's collaborate
+						</div>
+					</AnchorLink>
+				</motion.div>
+				<motion.div
+					className='flex mt-5 justify-center md:justify-start'
+					initial='hidden'
+					whileInView='visible'
+					viewport={{ once: true, amount: 0.5 }}
+					transition={{ delay: 0.2, duration: 0.5 }}
+					variants={{
+						hidden: { opacity: 0, x: -50 },
+						visible: { opacity: 1, x: 0 },
+					}}>
+					<MediaIcons />
+				</motion.div>
+			</div>
+		</section>
+	);
+};
+
+export default Landing;
