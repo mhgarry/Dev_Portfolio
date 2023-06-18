ea2b0f2e71ba9af7a79d1c7182937f365e3817d4
Matt <mhgarry92@gmail.com>
2023-06-18 14:59:25 -0400

    setup vite config to redo portfolio repo securely

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+
+	return defineConfig({
+		plugins: [react()],
+		define: {
+			"process.env": env,
+		},
+	});
+});
