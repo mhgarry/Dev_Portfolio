11a3a50f6ddf9ff7d215dab4e0d18a12abde4aee
Matt <mhgarry92@gmail.com>
2023-06-18 14:55:17 -0400

    fixed tailwind file

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,43 +1,33 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ["./src/**/*.{html,js,jsx}"],
-	mode: "jit",
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
 			colors: {
-				"deep-blue": "#010026",
-				blue: "#2CBCE9",
-				red: "#DC4492",
-				yellow: "#FDCC49",
-				grey: "#ededed",
-				"dark-grey": "#757575",
+				"deep-blue": "#0D1B2A",
+				turquoise: "#1DBACC",
+				"dark-gray": "#1D2330",
+				yellow: "#FFD700",
+				pink: "#ED3192",
+				"light-pink": "#FFC0CB",
+				"primary-blue": "#087FBF",
+				"light-gray": "#E0E0E0",
+				white: "#FFFFFF",
+				black: "#000000",
 				"opaque-black": "rgba(0,0,0,0.35)",
 			},
-			backgroundImage: (theme) => ({
-				"gradient-rainbow":
-					"linear-gradient(81.66deg, #00B5EE 7.21%, #FF45A4 45.05%, #FFBA00 78.07%)",
-
-				"gradient-rainblue":
-					"linear-gradient(90deg, #24CBFF 14.53%, #FC59FF 69.36%, #FFBD0C 117.73%)",
-			}),
+			screens: {
+				xs: "480px",
+				ss: "620px",
+				sm: "768px",
+				md: "1060px",
+				lg: "1280px",
+				xl: "1440px",
+				xxl: "1920px",
+			},
 			fontFamily: {
-				playfair: ["Playfair Display", "serif"],
-				opensans: ["Open Sans", "sans-serif"],
+				monospace: "VT323, monospace",
 			},
-			content: {
-				brush: "url('./assets/brush.png')",
-				person1: "url('./assets/person-1.png')",
-				person2: "url('./assets/person-2.png')",
-				person3: "url('./assets/person-3.png')",
-			},
-		},
-		screens: {
-			xs: "480px",
-			ss: "620px",
-			sm: "768px",
-			md: "1060px",
-			lg: "1200px",
-			xl: "1700px",
 		},
 	},
 	plugins: [],
